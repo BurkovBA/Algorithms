8f9537d0bbc60934e1f50eb29cbbec59f0fc0ee6
Boris A. Burkov <vasjaforutube@gmail.com>
2018-06-30 22:30:25 +0100

    Written dijkstra.

--- dijkstra/dijkstra.js
+++ dijkstra/dijkstra.js
@@ -1,27 +1,29 @@
+/**
+ * @param {object} graph - { node1: {node2: 2.4, node3: null}, node2: {node1: null, node3: 1.3} }
+ */
 function dijkstra (graph, origin, destination) {
-  let distances = {}; // {node: distance}
+  let reachedNodes = {}; // {node: distance}
 
   let reachableEdges = origin.edges.slice();
   let distance = 0; // this distance will monotonously increase
-  while (!distances.hasOwnProperty(destination)) {
+  while (!reachedNodes.hasOwnProperty(destination)) {
 
-    // find the closest reachable nearestEdge, increase the distance
-    let nearestEdge = stack.reduce((minimum, current) => {
-      if (!distances.hasOwnProperty(current.destination)) {
-        return current.distance < minimum.distance ? current : minimum;
-      } else {
-        return minimum;
-      }
+    // find the closest reachable edge, increase the distance
+    let nearestEdge = reachableEdges.reduce((minimum, current) => {
+        return current.distance + reachedNodes[current] < minimum.distance + reachedNodes[minimum] ? current : minimum;
     }, reachableEdges[0]);
 
+    // if new reached node, add it, increase the distance
+    if (!reachedNodes.hasOwnProperty(nearestEdge.destination)) {
+      reacedNodes[nearestEdge.destination] = nearestEdge.distance + distance;
+      nearestEdge.destination.edges.map((edge) => {
+        if (!reachedNodes.hasOwnProperty(edge.destination)) {
+          reachableEdges.push(edge);
+        }
+      });
+    }
     distance = distance + nearestEdge.distance;
 
-    // add edges, starting at nearestEdge.destination, to reachableEdges
-    nearestEdge.destination.edges.map((edge) => {
-      if (!distances.hasOwnProperty(edge.destination)) {
-        reachableEdges.push(edge);
-      }
-    });
-
+    if (destination in reachableEdges) return reachedNodes;
   }
 }
